00335850881679ad896bf630f36c056bae95cba1
Буковский Юрий <bukovskiy.yuriy@gmail.com>
2017-04-26 12:45:35 +0300

    bugfix

--- lib/bitcoin/protocol/alert.rb
+++ lib/bitcoin/protocol/alert.rb
@@ -33,8 +33,17 @@ module Protocol
       set_sub_ver,       payload = Bitcoin::Protocol.unpack_var_string_array(payload)
       priority,          payload = payload.unpack("Va*")
       comment,           payload = Bitcoin::Protocol.unpack_var_string(payload)
-      status_bar,        payload = Bitcoin::Protocol.unpack_var_string(payload)
-      reserved,          payload = Bitcoin::Protocol.unpack_var_string(payload)
+      # fix:
+      # require 'bitcoin'
+      # Bitcoin::Protocol::Alert.parse(["60010000000000000000000000ffffff7f00000000ffffff7ffeffff7f01ffffff7f00000000ffffff7f00ffffff7f002f555247454e543a20416c657274206b657920636f6d70726f6d697365642c2075706772616465207265717569726564004630440220653febd6410f470f6bae11cad19c48413becb1ac2c17f908fd0fd53bdc3abd5202206d0e9c96fe88d4a0f01ed9dedae2b6f9e00da94cad0fecaae66ecf689bf71b50"].pack('H*'))
+      if payload == "" then
+        status_bar = ""
+        reserved = ""
+      else
+        status_bar,        payload = Bitcoin::Protocol.unpack_var_string(payload)
+        reserved,          payload = Bitcoin::Protocol.unpack_var_string(payload)
+      end
+
 
       values = [ version, relay_until, expiration, id, cancel, set_cancel, min_ver, max_ver, set_sub_ver, priority, comment, status_bar, reserved ]
 
